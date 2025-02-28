d25b7fb9517f3a575ed4dbedfd2cc8dd6c8f2ec7
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-02-28 17:26:36 -0300

    Update SecurityConfig.java

--- src/main/java/com/maestria/agenda/config/SecurityConfig.java
+++ src/main/java/com/maestria/agenda/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -51,5 +53,11 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", corsConfig); // Aplica a configuração para todas as rotas
 
         return source;
+    }
+
+    // Criando o PasswordEncoder para ser injetado
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Usando o BCryptPasswordEncoder para segurança das senhas
     }
 }
