58b47617a5d931f4648178c9839d829f2f034ca7
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-02-28 16:56:50 -0300

    Update JwtAuthenticationFilter.java

--- src/main/java/com/maestria/agenda/config/JwtAuthenticationFilter.java
+++ src/main/java/com/maestria/agenda/config/JwtAuthenticationFilter.java
@@ -6,13 +6,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter
