12259f2f1d386a7d131a29c153ef7e673adbf5ea
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-01 10:57:44 -0300

    Update SecurityConfig.java

--- src/main/java/com/maestria/agenda/config/SecurityConfig.java
+++ src/main/java/com/maestria/agenda/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.maestria.agenda.config;
 
 import com.maestria.agenda.profissional.CustomUserDetailsService;
-import com.maestria.agenda.security.JwtAuthenticationFilter;
+import com.maestria.agenda.config.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
