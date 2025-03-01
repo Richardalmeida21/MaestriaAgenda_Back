12f4d1e9655b7572f29c4ba5b03db70ab7e5b2da
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-01 15:27:25 -0300

    Update DadosCadastroAgendamento.java

--- src/main/java/com/maestria/agenda/agendamento/DadosCadastroAgendamento.java
+++ src/main/java/com/maestria/agenda/agendamento/DadosCadastroAgendamento.java
@@ -1,19 +1,16 @@
 package com.maestria.agenda.agendamento;
 
-import com.maestria.agenda.cliente.DadosCliente;
-import com.maestria.agenda.profissional.DadosProfissional;
 import com.maestria.agenda.servicos.Servicos;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record DadosCadastroAgendamento(
-        @NotNull DadosCliente cliente,
-        @NotNull DadosProfissional profissional,
+        @NotNull Long clienteId,  // Agora usa ID do cliente
+        @NotNull Long profissionalId, // Agora usa ID do profissional
         @NotNull Servicos servico,
         @NotNull @FutureOrPresent LocalDate data,
         @NotNull LocalTime hora
-) {
-}
+) {}
