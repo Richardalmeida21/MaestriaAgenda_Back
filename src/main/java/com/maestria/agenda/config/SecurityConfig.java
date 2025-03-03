745c6b5453d5e2da489ab87a481e87484124e2c9
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-03 20:58:19 -0300

    Update SecurityConfig.java

--- src/main/java/com/maestria/agenda/config/SecurityConfig.java
+++ src/main/java/com/maestria/agenda/config/SecurityConfig.java
@@ -7,12 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.List;
 
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -47,7 +45,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(List.of(
             "https://maestria-agenda.netlify.app", 
