29a09ad2d9d6687523cca855a5d938ea7cb39f47
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-03 21:56:04 -0300

    correção

--- src/main/java/com/maestria/agenda/config/SecurityConfig.java
+++ src/main/java/com/maestria/agenda/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.maestria.agenda.config;
 
+import com.maestria.agenda.profissional.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
