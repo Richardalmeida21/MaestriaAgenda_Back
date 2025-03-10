f8fc4dae3ff67ec0c9d7f6e6347e75285e9f1c7a
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-10 11:28:24 -0300

    Update AgendamentoController.java

--- src/main/java/com/maestria/agenda/controller/AgendamentoController.java
+++ src/main/java/com/maestria/agenda/controller/AgendamentoController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
