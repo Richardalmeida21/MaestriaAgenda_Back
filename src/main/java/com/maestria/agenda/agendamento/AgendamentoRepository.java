ff7a8d0481b8d7d9ed20c724d5599cbf50002e1c
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-10 11:28:06 -0300

    Update AgendamentoRepository.java

--- src/main/java/com/maestria/agenda/agendamento/AgendamentoRepository.java
+++ src/main/java/com/maestria/agenda/agendamento/AgendamentoRepository.java
@@ -3,6 +3,7 @@ package com.maestria.agenda.agendamento;
 import com.maestria.agenda.cliente.Cliente;
 import com.maestria.agenda.profissional.Profissional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
