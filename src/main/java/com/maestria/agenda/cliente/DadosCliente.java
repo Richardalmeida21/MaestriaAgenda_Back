6a90d56af9f71f4b0c3f9d2c1556f7f79c31bba9
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-14 11:25:48 -0300

    Update DadosCliente.java

--- src/main/java/com/maestria/agenda/cliente/DadosCliente.java
+++ src/main/java/com/maestria/agenda/cliente/DadosCliente.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record DadosCliente(
-        @NotBlank String nome,
-        @Email @NotBlank String email,
-        @NotBlank String telefone
+        @NotBlank(message = "Nome não pode ser vazio") String nome,
+        @Email(message = "Email inválido") @NotBlank(message = "Email não pode ser vazio") String email,
+        @NotBlank(message = "Telefone não pode ser vazio") String telefone
 ) {
 }
