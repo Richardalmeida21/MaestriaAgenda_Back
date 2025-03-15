f0a26591b45337c3a0144ff882daedc19f31a4a3
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-15 14:37:37 -0300

    Update AgendamentoController.java

--- src/main/java/com/maestria/agenda/controller/AgendamentoController.java
+++ src/main/java/com/maestria/agenda/controller/AgendamentoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/agendamento")
