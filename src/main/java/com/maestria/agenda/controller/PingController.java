4d8e52b4734a9a84f76a701a119d4184b9c0883b
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-28 10:12:05 -0300

    Update PingController.java

--- src/main/java/com/maestria/agenda/controller/PingController.java
+++ src/main/java/com/maestria/agenda/controller/PingController.java
@@ -1,13 +1,20 @@
-package com.maestria.agenda.controller; 
+package com.maestria.agenda.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
 
+    // Criação do Logger
+    private static final Logger logger = LoggerFactory.getLogger(PingController.class);
+
     @GetMapping("/ping")
     public String ping() {
+        // Adicionando o log
+        logger.info("Requisição recebida no endpoint /ping");
         return "API está funcionando!";
     }
 }
