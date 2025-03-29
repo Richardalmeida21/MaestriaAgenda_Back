d0100c854f610dba408436a47a9e3a1cfb5e67c2
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-03-29 14:22:33 -0300

    Update AgendamentoFixoRepository.java

--- src/main/java/com/maestria/agenda/agendamento/AgendamentoFixoRepository.java
+++ src/main/java/com/maestria/agenda/agendamento/AgendamentoFixoRepository.java
@@ -12,4 +12,6 @@ public interface AgendamentoFixoRepository extends JpaRepository<AgendamentoFixo
     // Busca agendamentos fixos pelo profissional e dia do mês
     List<AgendamentoFixo> findByProfissionalAndDiaDoMes(Profissional profissional, int diaDoMes);
     List<AgendamentoFixo> findByProfissional(Profissional profissional);
+    // Adicione isto ao AgendamentoFixoRepository
+    List<AgendamentoFixo> findByProfissionalAndDiaDoMes(Profissional profissional, int diaDoMes);
 }
