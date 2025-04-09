8361ff1d34dd45b43c8810c34385d4c614dfb54a
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-04-09 11:44:27 -0300

    Update ComissaoService.java

--- src/main/java/com/maestria/agenda/financeiro/ComissaoService.java
+++ src/main/java/com/maestria/agenda/financeiro/ComissaoService.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import java.time.LocalDateTime;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
