3a92f5c328410fd773f2eac8c4151cca3fd81e5d
Richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-04-10 16:35:59 -0300

    ok

--- src/main/java/com/maestria/agenda/financeiro/ExpenseResponseDTO.java
+++ src/main/java/com/maestria/agenda/financeiro/ExpenseResponseDTO.java
@@ -1,8 +1,6 @@
 package com.maestria.agenda.financeiro;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -10,8 +8,6 @@ import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ExpenseResponseDTO {
     private Long id;
     private String description;
@@ -22,8 +18,6 @@ public class ExpenseResponseDTO {
     private Boolean isFixo;
     private Integer dayOfMonth;
     private LocalDate endDate;
-
-    public ExpenseResponseDTO() {}
 
     // Construtor completo com todos os campos
     public ExpenseResponseDTO(Long id, String description, String category, LocalDate date, Double amount, Boolean paid, Boolean isFixo, Integer dayOfMonth, LocalDate endDate) {
@@ -51,80 +45,7 @@ public class ExpenseResponseDTO {
         this.endDate = null;
     }
 
-    // Getters e Setters existentes
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public String getDateFormatted() {
-        return date.format(DateTimeFormatter.ISO_DATE);
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public Boolean getPaid() {
-        return paid;
-    }
-
-    public void setPaid(Boolean paid) {
-        this.paid = paid;
-    }
-
-    public Boolean getIsFixo() {
-        return isFixo;
-    }
-
-    public void setIsFixo(Boolean isFixo) {
-        this.isFixo = isFixo;
-    }
-
-    public Integer getDayOfMonth() {
-        return dayOfMonth;
-    }
-
-    public void setDayOfMonth(Integer dayOfMonth) {
-        this.dayOfMonth = dayOfMonth;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+        return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 }
