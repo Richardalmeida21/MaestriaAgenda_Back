592fa42d3e1964187155887cc49b01c708d4fd9b
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2026-02-05 11:40:39 -0300

    retirando obrigatorieade de email

--- src/main/java/com/maestria/agenda/cliente/DadosCliente.java
+++ src/main/java/com/maestria/agenda/cliente/DadosCliente.java
@@ -1,11 +1,9 @@
 package com.maestria.agenda.cliente;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record DadosCliente(
-        @NotBlank(message = "Nome não pode ser vazio") String nome,
-        @Email(message = "Email inválido") @NotBlank(message = "Email não pode ser vazio") String email,
-        @NotBlank(message = "Telefone não pode ser vazio") String telefone
-) {
+                @NotBlank(message = "Nome não pode ser vazio") String nome,
+                String email,
+                String telefone) {
 }
